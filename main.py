81b55bf313f9808a1b05e1930823612de73633ec
riyapathania <154575279+riyapathania@users.noreply.github.com>
2024-01-23 05:15:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+df = pd.read_csv('creditcard.csv')
+print(df.isnull().sum())
+X = df.drop('Class', axis=1)
+y = df['Class']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+rf_classifier = RandomForestClassifier(random_state=42)
+rf_classifier.fit(X_train_scaled, y_train)
+y_pred = rf_classifier.predict(X_test_scaled)
+
+accuracy = accuracy_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+class_report = classification_report(y_test, y_pred)
+
+print(f'Accuracy: {accuracy}')
+print(f'Confusion Matrix:\n{conf_matrix}')
+print(f'Classification Report:\n{class_report}')
+
+print("Missing Values:\n", df.isnull().sum())
